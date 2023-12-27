9432ac0ecda0acfecc449a6838764ccf15a32c95
MrBocch <jorgealberto1436@gmail.com>
2023-12-26 23:25:24 -0600

    change how converted mp4 files into mp3, now it will download all the videos then change their extension once all downloaded

--- main.py
+++ main.py
@@ -19,23 +19,40 @@ def download(links):
         except: 
             print(f"Error on this link\n{link}")
         else:
-            yt.title = sanatizeTitle(yt.title)
-            print(f"Installing {yt.title}.mp3")
+            print(f"Downloading {yt.title}")
             yt.streams.get_audio_only().download()
-            renameToMp3(yt.title)
-            # yt.streams.filter(only_audio=True).first().download()
+
+    renameAlltoMp3()
+
+def renameAlltoMp3():
+    for f in os.listdir():
+        sp = f.split(".")
+        sp[1] = ".mp3"
+        try:
+            os.rename(f"{f}", f'{"".join(sp)}')
+        except:
+            print("Something went wrong in renameing file")
 
 def renameToMp3(title):
-    os.rename(f"{title}.mp4", f"{title}.mp3")
+    try: 
+        os.rename(f"{title}.mp4", f"{title}.mp3")
+    except:
+        print(f"\n\nError at {title}")
+        print("having issues with these types of titles")
+        print("try renaming to mp3 manually\n")
 
 def sanatizeTitle(title):
     # this is a very interesting issue
     # they are certain characters that are not allowed to include in file name
     # but they are allowed in a title of a youtube video, so i have to replace that
-    illegal = r'[^a-zA-Z0-9]'
-    return re.sub(illegal, '_', title)
+    
+    # this does not include other alfabets or fonts in youtube titles
+    # illegal = r'[^a-zA-Z0-9]' 
+    # i decided not use this because there could be literally anything in the 
+    # youtube title including non-ascii characters wich is a serious pain
+    illegal = r'[^\w\s\.\-]'
+    return re.sub(illegal, '_', title, flags=re.UNICODE)
    
-    return title
 
 def main():
     setup()
